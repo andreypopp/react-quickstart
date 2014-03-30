16c7b74090ddf7c10079c089cdf55d7560e8b41f
Andrey Popp <8mayday@gmail.com>
2014-03-30 13:51:25 +0400

    Update to a new react-router-component

--- client.js
+++ client.js
@@ -30,18 +30,37 @@ var MainPage = React.createClass({
 var UserPage = React.createClass({
   mixins: [ReactAsync.Mixin],
 
-  getInitialStateAsync: function(cb) {
+  getUserInfo: function(username, cb) {
     superagent.get(
-      'http://localhost:3000/api/users/' + this.props.username,
+      'http://localhost:3000/api/users/' + username,
       function(err, res) {
         cb(err, res ? res.body : null);
       });
   },
 
+  getInitialStateAsync: function(cb) {
+    this.getUserInfo(this.props.username, cb);
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if (this.props.username !== nextProps.username) {
+      this.getUserInfo(nextProps.username, function(err, info) {
+        if (err) {
+          throw err;
+        }
+        this.setState(info);
+      }.bind(this));
+    }
+  },
+
   render: function() {
+    var otherUser = this.props.username === 'doe' ? 'ivan' : 'doe';
     return (
       <div className="UserPage">
         <h1>Hello, {this.state.name}!</h1>
+        <p>
+          Go to <Link href={"/users/" + otherUser}>/users/{otherUser}</Link>
+        </p>
         <p><Link href="/">Logout</Link></p>
       </div>
     );
